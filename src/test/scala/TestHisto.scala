9aa8a06584e1ed874f0295373a82c482283dd655
Neel Smith <dnsmith.neel@gmail.com>
2016-06-04 11:33:40 -0400

    Add test on full corpus.

--- src/test/scala/TestHisto.scala
+++ src/test/scala/TestHisto.scala
@@ -2,6 +2,9 @@ package ecevit
 
 import org.specs2.mutable._
 import scala.io.Source
+import java.io.File
+import java.io.PrintWriter
+import scala.collection.immutable.ListMap
 
 class  HistogramTest extends Specification {
 
@@ -16,5 +19,29 @@ class  HistogramTest extends Specification {
     }
    }
 
+
+
+   "A Histogram object" should {
+     "map even a whole corpus of tokens to counts" in {
+       val resDir = new File("ocr")
+       val tokens = Tokenizer.tokenizeFileTree(resDir)
+       val hist = Histo.histogram(tokens)
+       println ("Input size (tokens): " + tokens.size)
+       println ("Number values (distinct tokens): " + hist.values.size)
+       println("Max occurrences: " + hist.values.max)
+
+       val sortedByCount = hist.toSeq.sortBy(_._2).reverse
+
+       // save top 100 in a file just for fun
+       val outWriter = new PrintWriter("corpus-freqs.txt")
+       for (wd <- sortedByCount.take(100)) {
+         outWriter.println(wd)
+       }
+       outWriter.close()
+
+       tokens.size must_== 542451
+       hist.values.size must_== 110552
+     }
+   }
 
 }
